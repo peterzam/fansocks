0bb8d498a9a75428b524f5f6507a3aa380b8a982
Peter Zam <mail@peterzam.dev>
2022-07-11 11:57:46 +0630

    first commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module codeberg.org/peterzam/fansocks
+
+go 1.18
+
+require (
+	codeberg.org/peterzam/socks5 v0.0.0-20220710174902-2f6b98018b3e
+	golang.org/x/net v0.0.0-20220708220712-1185a9018129
+)
---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"net"
+
+	"codeberg.org/peterzam/socks5"
+	"golang.org/x/net/proxy"
+)
+
+var (
+	bindAddr    = flag.String("bind", "0.0.0.0:1080", "Bind Address for SOCKS5")
+	csvfilepath = flag.String("csv", "socks.csv", "SOCKS5 server list csv file")
+)
+
+func main() {
+
+	var t []proxy.Dialer
+	f, err := ioutil.ReadFile(*csvfilepath)
+	if err != nil {
+		fmt.Println("----------------")
+		fmt.Println("CSV file read error")
+		fmt.Println("----------------")
+		panic(err)
+	}
+	sockstrings := bytes.Split(f, []byte("\n"))
+
+	for _, i := range sockstrings {
+		j, _ := proxy.SOCKS5("tcp", string(i), &proxy.Auth{}, nil)
+		t = append(t, j)
+	}
+
+	var i int
+	server, _ := socks5.New(&socks5.Config{
+		Dial: func(ctx context.Context, network, addr string) (net.Conn, error) {
+			if ctx.Done() == nil {
+				i = rand.Intn(len(t))
+			}
+			return t[i].Dial(network, addr)
+		},
+	})
+	if err := server.ListenAndServe("tcp", *bindAddr); err != nil {
+		fmt.Println("----------------")
+		fmt.Println("SOCKS5 server cannot be started at : ", *bindAddr)
+		fmt.Println("----------------")
+		panic(err)
+	}
+}
